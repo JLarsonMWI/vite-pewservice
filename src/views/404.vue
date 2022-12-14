ab011935637768bedc5bc990f4400038ca1f5ba3
John Larson <jl@buildmidwestern.com>
2022-12-14 05:06:04 -0600

    chore: updated 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,32 +1,33 @@
 <template>
   <div
-    class="min-h-full bg-gray-900 px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    class="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
     <div class="mx-auto max-w-max">
       <main class="sm:flex">
         <p
-          class="text-4xl font-bold tracking-tight bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text text-transparent sm:text-5xl">
+          class="bg-gradient-to-tr from-rose-500 to-rose-500 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl">
           404
         </p>
-        <div class="sm:ml-6">
+        <div
+          class="bg-gradient-to-tr from-rose-500 to-rose-500 bg-clip-text sm:ml-6">
           <div class="sm:border-l sm:border-gray-200 sm:pl-6">
             <h1
-              class="text-4xl font-bold tracking-tight text-white sm:text-5xl">
+              class="text-4xl font-bold tracking-tight text-transparent sm:text-5xl">
               Page not found
             </h1>
-            <p class="mt-1 text-base text-gray-300">
+            <p class="mt-1 text-base text-transparent">
               Please check the URL in the address bar and try again.
             </p>
           </div>
           <div
-            class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            class="mt-10 flex space-x-3 bg-gradient-to-tr from-rose-500 to-rose-500 bg-clip-text sm:border-l sm:border-transparent sm:pl-6">
             <a
-              href="/home"
-              class="inline-flex items-center rounded-md border border-transparent px-4 py-2 text-lg font-bold text-cyan-200 hover:text-gray-300"
+              href="/"
+              class="inline-flex items-center rounded-md border border-transparent px-4 py-2 text-lg font-bold text-transparent hover:italic"
               >Go back home</a
             >
             <a
               href="/dashboard"
-              class="inline-flex items-center rounded-md border border-transparent px-4 py-2 text-lg font-medium text-cyan-200 hover:text-gray-300"
+              class="inline-flex items-center rounded-md border border-transparent px-4 py-2 text-lg font-medium text-transparent hover:italic"
               >View your dashboard</a
             >
           </div>
